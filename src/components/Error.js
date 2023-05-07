b42e36eec4c45c77dc65ed1f31524c730d2d1ba2
Mahmouud74 <mahmouudmoohamed@gmail.com>
2023-05-07 22:52:01 +0200

    add error component

---
+++ src/components/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div className='text-light d-flex justify-content-center bg-light'>
+            <div className='alert alert-danger '>
+                NOT FOUND
+            </div>
+        </div>
+    );
+};
+
+export default Error;
